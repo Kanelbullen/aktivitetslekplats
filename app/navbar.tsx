b393b6fe4c7ea28b349e8e77a0841c0999e8a91c
WilliamLundqvist <wille1000000@gmail.com>
2024-09-05 10:20:17 +0200

    added navbar

---
+++ app/navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=' w-full flex flex-row justify-between items-center bg-gray-100 h-14'>
+        <div>
+         dsda
+        </div>
+
+        <div>
+dadsda
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
