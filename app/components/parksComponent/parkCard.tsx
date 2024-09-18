139ce6dae5af5446d02535f31da30bcade5fbca1
WilliamLundqvist <wille1000000@gmail.com>
2024-09-18 11:21:32 +0200

    get all id:s

--- app/components/parksComponent/parkCard.tsx
+++ app/components/parksComponent/parkCard.tsx
@@ -1,9 +1,10 @@
+import { notFound } from 'next/navigation';
 import { createClient } from '@vercel/edge-config';
 import Navbar from '@/app/components/navbar';
 import Footer from '@/app/components/footer';
 import StarRating from '@/app/components/StarRating';
 import Link from 'next/link'; // För att skapa länkar till enskilda parksidor
- const edgeConfig = createClient(process.env.NEXT_PUBLIC_EDGE_CONFIG!);
+const edgeConfig = createClient(process.env.EDGE_CONFIG!); 
 
 
 interface ParkProps {
@@ -24,6 +25,28 @@ function isParkProps(park: unknown): park is ParkProps {
     'rating' in park && typeof (park as ParkProps).rating === 'number';
 }
 
+async function getAllParks() {
+  // Hämta listan med alla park-ID:n från Edge Config
+  const parkIds = (await edgeConfig.getAll()) as number[];
+
+  const parks: ParkProps[] = [];
+
+  // Hämta varje park med sitt ID
+  for (const id of parkIds) {
+    const parkData = await edgeConfig.get(`park_${id}`);
+
+    // Kontrollera att parkData är av typen ParkProps
+    if (isParkProps(parkData)) {
+      parks.push(parkData);
+    } else {
+      console.warn(`Park data för id ${id} är inte av förväntad typ`);
+    }
+  }
+
+  return parks;
+}
+
+
 export default async function ParkListPage() {
   try {
     // Hämta alla parker
@@ -39,13 +62,13 @@ export default async function ParkListPage() {
             <h2 className='text-2xl text-center text-[#33582D] mb-6'>Alla Parker</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
               {allParks.map((park: ParkProps) => (
-                <div key={park.id} className='border rounded-lg shadow-lg p-4'>
-                  <img className='w-full h-48 object-cover mb-4' src={park.image_url} alt={park.name} />
-                  <h3 className='text-xl font-bold mb-2 text-center'>{park.name}</h3>
+                <div key={park!.id} className='border rounded-lg shadow-lg p-4'>
+                  <img className='w-full h-48 object-cover mb-4' src={park!.image_url} alt={park!.name} />
+                  <h3 className='text-xl font-bold mb-2 text-center'>{park!.name}</h3>
                   <div className='text-center mb-2'>
-                    <StarRating rating={park.rating} />
+                    <StarRating rating={park!.rating} />
                   </div>
-                  <Link href={`/park/${park.id}`}>
+                  <Link href={`/park/${park!.id}`}>
                     <a className='text-blue-500 text-center block'>Läs mer</a>
                   </Link>
                 </div>
@@ -64,23 +87,4 @@ export default async function ParkListPage() {
 }
 
 // Funktion för att hämta alla parker
-async function getAllParks() {
-  // Hämta listan med alla park-ID:n från Edge Config
-  const parkIds = (await edgeConfig.get('park_list')) as number[];
 
-  const parks: ParkProps[] = [];
-
-  // Hämta varje park med sitt ID
-  for (const id of parkIds) {
-    const parkData = await edgeConfig.get(`park_${id}`);
-
-    // Kontrollera att parkData är av typen ParkProps
-    if (isParkProps(parkData)) {
-      parks.push(parkData);
-    } else {
-      console.warn(`Park data för id ${id} är inte av förväntad typ`);
-    }
-  }
-
-  return parks;
-}
