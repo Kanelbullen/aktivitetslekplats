b03743565ee90b3ffab0d72e93ba87e9f196a81e
Kanelbullen <84346060+Kanelbullen@users.noreply.github.com>
2024-09-18 10:49:19 +0200

    Added goole maps

--- app/components/parksComponent/parkmap.tsx
+++ app/components/parksComponent/parkmap.tsx
@@ -1,11 +1,37 @@
-
+'use client'
+import { Loader } from "@googlemaps/js-api-loader";
+import React, { useEffect} from "react";
 
 const parkmap = () => {
 
+  const mapRef = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const initMap = async () => {
+      const loader = new Loader({
+        apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
+        version: 'weekly'
+      });
+
+      const { Map } = await loader.importLibrary('maps');
+      const position = {
+        lat: 57.7210,
+        lng: 12.9398,
+      }
+
+      const mapOptions: google.maps.MapOptions = {
+        center: position,
+        zoom: 15,
+        mapId: '3164088431'
+      }
+
+      const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
+    }
+    initMap();
+  }, []);
+
   return (
-    <div>
-      
-    </div>
+    <div style={{ height: '600px', width: '400px',  }} ref={mapRef}/>
   );
 };
 
