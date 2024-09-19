fcf3debc0f2a6c19c8350d32af5c507742ea5df9
WilliamLundqvist <wille1000000@gmail.com>
2024-09-19 13:01:28 +0200

    comment

--- app/components/parksComponent/parkCardList.tsx
+++ app/components/parksComponent/parkCardList.tsx
@@ -7,6 +7,7 @@ const ParkCardList = () => {
 
   return (
     <div className='my-5 flex flex-col gap-2'>
+      {/* I varje ParkCard kan du föra in värderna från databasen. Skulle kanske behöva en For loop föra alla ID:s i databasen och loopa genom alla id:s med korressponderande info */}
       <ParkCard id={1} name='Park 1' distance={12} img={ParkImage} description='Very nice park'/>
       <ParkCard id={2} name='Park 2' distance={12} img={ParkImage} description='Very nice park'/>
       <ParkCard id={3} name='Park 3' distance={12} img={ParkImage} description='Very nice park'/>
